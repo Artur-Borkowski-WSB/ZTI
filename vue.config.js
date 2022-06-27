46d5addcb3c7d7d9db85b60132dc26a946577159
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2022-06-27 18:49:13 +0200

    config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production' ? '/zti/' : '/',
+
+  pwa: {
+    themeColor: '#ff4444',
+    manifestOptions: {
+      background_color: '#FFFFFF'
+    }
+  }
 }
